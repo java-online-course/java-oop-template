9a1940566b990926c3eaf89d42518646ac54c332
Kirill <kirill_nn@inbox.ru>
2019-12-13 22:21:13 +0300

    Остались два метода. Там, где простой переброс, сделал

--- src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
@@ -9,39 +9,58 @@ public class SimpleSchoolBookService implements BookService {
     private BookRepository<SchoolBook> schoolBookBookRepository;
     private AuthorService authorService;
 
+    public SimpleSchoolBookService() {
+    }
+
     public SimpleSchoolBookService(BookRepository<SchoolBook> schoolBookBookRepository, AuthorService authorService) {
         this.schoolBookBookRepository = schoolBookBookRepository;
         this.authorService = authorService;
     }
 
-    public SimpleSchoolBookService() {
-    }
-
+    /**
+     * Метод должен сохранять книгу.
+     * <p>
+     * Перед сохранением книги нужно проверить, сохранен ли такой автор в базе авторов.
+     * То есть вы должен взять имя и фамилию автора из книги и обратиться к сервису авторов и узнать о наличии такого автора.
+     * Напомню, что мы считаем, что двух авторов с одинаковыми именем и фамилией быть не может.
+     * <p>
+     * Если такой автор сущесвует (сохранен) - значит можно сохранять и книгу.
+     * Если же такого автора в базе нет, значит книгу сохранять нельзя.
+     * <p>
+     * Соответственно, если книга была успешно сохранена - метод возвращает true, если же книга не была сохранена - метод возвращает false.
+     */
     @Override
     public boolean save(Book book) {
-        return false;
+        return true;
     }
 
     @Override
     public Book[] findByName(String name) {
-        return new Book[0];
+        return schoolBookBookRepository.findByName(name);
     }
 
     @Override
     public int getNumberOfBooksByName(String name) {
-        return 0;
+        return schoolBookBookRepository.findByName(name).length;
     }
 
     @Override
     public boolean removeByName(String name) {
-        return false;
+        return schoolBookBookRepository.removeByName(name);
     }
 
     @Override
     public int count() {
-        return 0;
+        return schoolBookBookRepository.count();
     }
 
+    /**
+     * Метод должен возвращать автора книги по названию книги.
+     * <p>
+     * То есть приждется сходить и в репозиторий с книгами и в сервис авторов.
+     * <p>
+     * Если такой книги не найдено, метод должен вернуть null.
+     */
     @Override
     public Author findAuthorByBookName(String name) {
         return null;
