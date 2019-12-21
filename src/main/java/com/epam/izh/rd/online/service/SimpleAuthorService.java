ae7fbc5a7154ff96e757beba9c09a41ab28910bf
Bulygin <jbulygin@yandex.ru>
2019-12-21 10:18:15 +0300

    First commit

---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleAuthorService.java
@@ -0,0 +1,37 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.service.AuthorService;
+
+public class SimpleAuthorService implements AuthorService {
+
+  private AuthorRepository authorRepository;
+
+  public SimpleAuthorService() {
+
+  }
+
+  public SimpleAuthorService(AuthorRepository authorRepository) {
+    this.authorRepository = authorRepository;
+  }
+
+  @Override
+  public boolean save(Author author) {
+    return false;
+  }
+
+  @Override
+  public Author findByFullName(String name, String lastname) {
+    return null;
+  }
+
+  @Override
+  public boolean remove(Author author) {
+    return false;
+  }
+
+  @Override
+  public int count() {
+    return 0;
+  }
+}
