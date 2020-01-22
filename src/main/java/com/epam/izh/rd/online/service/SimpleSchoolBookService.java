f4146d165f4120903d7f941c7fd3b1c006c22f32
Violetta <violet.saharova@gmail.com>
2020-01-22 23:28:49 +0300

    реализация Задачи с книгами

---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
@@ -0,0 +1,61 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.entity.SchoolBook;
+import com.epam.izh.rd.online.repository.BookRepository;
+
+public class SimpleSchoolBookService implements BookService<SchoolBook>{
+
+    private BookRepository<SchoolBook> schoolBookBookRepository;
+    private AuthorService authorService;
+
+    public SimpleSchoolBookService() {
+    }
+
+    public SimpleSchoolBookService(BookRepository<SchoolBook> schoolBookBookRepository, AuthorService authorService) {
+        this.schoolBookBookRepository = schoolBookBookRepository;
+        this.authorService = authorService;
+    }
+
+    @Override
+    public boolean save(SchoolBook book) {
+        Author author = authorService.findByFullName(book.getAuthorName(), book.getAuthorLastName());
+        if (author != null)
+        {
+            schoolBookBookRepository.save(book);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public SchoolBook[] findByName(String name) {
+        return schoolBookBookRepository.findByName(name);
+    }
+
+    @Override
+    public int getNumberOfBooksByName(String name) {
+        return schoolBookBookRepository.findByName(name).length;
+    }
+
+    @Override
+    public boolean removeByName(String name) {
+        return schoolBookBookRepository.removeByName(name);
+    }
+
+    @Override
+    public int count() {
+
+        return schoolBookBookRepository.count();
+    }
+
+    @Override
+    public Author findAuthorByBookName(String name) {
+        SchoolBook[] books = schoolBookBookRepository.findByName(name); // список книг по этому имени книги
+        if (books.length > 0) {
+            return authorService.findByFullName(books[0].getAuthorName(), books[0].getAuthorLastName());}
+        else
+            return null;
+    }
+}
