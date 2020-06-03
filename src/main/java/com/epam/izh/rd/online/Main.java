6793ba3f25e0e240865154a5c533e31a0c216cf1
Alexander Savelyev <alexsave@gmail.com>
2020-06-03 11:27:23 +0300

    SimpleAuthorRepository is ready

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,8 +1,26 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.repository.SimpleAuthorRepository;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        SimpleAuthorRepository repository = new SimpleAuthorRepository();
+
+        repository.save(new Author("Alexander", "Pushkin", LocalDate.of(1799, 06, 06),"Russia"));
+        System.out.println(repository.findByFullName("AleXANDER", "PushkiN"));
+        System.out.println(repository.count());
+
+        repository.save(new Author("Alexander", "Pushkin", LocalDate.of(1799, 6, 6),"Russia"));
+        System.out.println(repository.count());
+
+        repository.save(new Author("Lev", "Tosltoy", LocalDate.of(1828, 9, 9),"Russia"));
+        System.out.println(repository.count());
+        System.out.println(repository.findByFullName("Le", "T"));
 
     }
 
--- src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -4,6 +4,9 @@ import com.epam.izh.rd.online.entity.Author;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.apache.commons.lang3.ArrayUtils.toArray;
 
 public class SimpleAuthorRepository implements AuthorRepository{
 
@@ -11,26 +14,26 @@ public class SimpleAuthorRepository implements AuthorRepository{
 
     @Override
     public boolean save(Author author) {
-        findByFullName(author.getName(), author.getLastName());
-        authors = (Author[]) Arrays.stream(authors).toArray();
-        return false;
+        if (findByFullName(author.getName(), author.getLastName()) != null) return false;
+        //authors = Arrays.stream(authors).toArray(Author[]::new);
+        authors = Stream.concat(Arrays.stream(authors), Stream.of(author)).toArray(Author[]::new);
+        return true;
     }
 
     @Override
     public Author findByFullName(String name, String lastName) {
-        name = name.toUpperCase();
-        lastName = name.toUpperCase();
         for (Author author : authors) {
-            if (author.getName().equals(name) && author.getLastName().equals(lastName)) return author;
+            if (author.getName().toUpperCase().equals(name.toUpperCase()) &&
+                    author.getLastName().toUpperCase().equals(lastName.toUpperCase())) return author;
         }
-        return Optional<Author>;
+        return null;
     }
 
     @Override
     public boolean remove(Author author) {
         int countBefore = count();
         authors = (Author[]) Arrays.stream(authors).filter(o -> !o.equals(author)).toArray();
-        return countBefore == count() ? false : true;
+        return countBefore != count();
     }
 
     @Override
