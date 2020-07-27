87cf9ac76594a0feaec2000c57db4fd0845ad94f
ymnya <ymnyaga@yandex.ru>
2020-07-27 22:48:33 +0300

    SimpleAuthorRepository is done

--- src/main/java/com/epam/izh/rd/online/entity/SchoolBook.java
+++ src/main/java/com/epam/izh/rd/online/entity/SchoolBook.java
@@ -21,4 +21,66 @@ import java.util.Objects;
  */
 public class SchoolBook extends Book {
 
+    private String authorName;
+    private String authorLastName;
+    private LocalDate publishDate;
+
+    public SchoolBook() {
+    }
+
+    public SchoolBook(int numberOfPages, String name, String authorName, String authorLastName, LocalDate publishDate) {
+        super(numberOfPages, name);
+        this.authorName = authorName;
+        this.authorLastName = authorLastName;
+        this.publishDate = publishDate;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        SchoolBook that = (SchoolBook) o;
+        return Objects.equals(authorName, that.authorName) &&
+                Objects.equals(authorLastName, that.authorLastName) &&
+                Objects.equals(publishDate, that.publishDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), authorName, authorLastName, publishDate);
+    }
+
+    @Override
+    public String toString() {
+        return "SchoolBook{" +
+                "authorName='" + authorName + '\'' +
+                ", authorLastName='" + authorLastName + '\'' +
+                ", publishDate=" + publishDate +
+                '}';
+    }
 }
---
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -0,0 +1,64 @@
+package com.epam.izh.rd.online.repository;
+
+import com.epam.izh.rd.online.entity.Author;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+
+public class SimpleAuthorRepository implements AuthorRepository {
+
+    private Author[] authors = null;
+
+    public SimpleAuthorRepository() {
+    }
+
+    @Override
+    public boolean save(Author author) {
+        if (findByFullName(author.getName(), author.getLastName()) != null) {
+            return false;
+        } else {
+            if (authors == null) {
+                authors = new Author[1];
+                authors[0] = author;
+            } else {
+                Author[] arrayCopy = Arrays.copyOf(authors, authors.length + 1);
+                arrayCopy[arrayCopy.length - 1] = author;
+                authors = arrayCopy;
+            }
+            return true;
+        }
+
+    }
+
+    @Override
+    public Author findByFullName(String name, String lastname) {
+        for (Author author : authors) {
+            if (author.getName().equals(name) && author.getLastName().equals(lastname)) {
+                return author;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean remove(Author author) {
+        if (findByFullName(author.getName(), author.getLastName()) == null) {
+            return false;
+        } else {
+            for (int i = 0; i < authors.length - 1; i++) {
+                if (findByFullName(author.getName(), author.getLastName()).equals(authors[i])) {
+                    Author[] arrayCopy = new Author[authors.length - 1];
+                    System.arraycopy(authors, 0, arrayCopy, 0, i);
+                    System.arraycopy(authors, i + 1, arrayCopy, i, authors.length - i - 1);
+                    authors = arrayCopy;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int count() {
+        return authors.length;
+    }
+}
