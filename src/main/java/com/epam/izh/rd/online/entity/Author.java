ce6f10f032fbebcb478d6f9f5c1a57e93e3eac4b
Airat_Salikhov <airatchaplin@gmail.com>
2020-09-09 10:14:26 +0300

    add Author, Book, SchoolBook.java

--- src/main/java/com/epam/izh/rd/online/entity/Author.java
+++ src/main/java/com/epam/izh/rd/online/entity/Author.java
@@ -2,6 +2,7 @@ package com.epam.izh.rd.online.entity;
 
 import java.time.LocalDate;
 import java.util.Objects;
+import java.util.SplittableRandom;
 
 /**
  * Класс содержащий информацию об авторе.
@@ -19,5 +20,76 @@ import java.util.Objects;
  * 6) Переопределить метод toString с выводом всех полей (не забывайте alt+inset)
  */
 public class Author {
+    private String name;
+    private String lastName;
+    private LocalDate birthdate;
+    private String country;
 
+    public Author() {
+    }
+
+    public Author(String name, String lastName, LocalDate birthdate, String country) {
+        this.name = name;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(getName(), author.getName()) &&
+                Objects.equals(getLastName(), author.getLastName()) &&
+                Objects.equals(getBirthdate(), author.getBirthdate()) &&
+                Objects.equals(getCountry(), author.getCountry());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getLastName(), getBirthdate(), getCountry());
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate=" + birthdate +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
