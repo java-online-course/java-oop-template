792c6b5538da12d258ecd75824915b4a57b52be6
Vlad <vladikigs@mail.ru>
2020-09-16 19:06:00 +0400

    Added implementation of the app structure

---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
@@ -0,0 +1,57 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.entity.SchoolBook;
+import com.epam.izh.rd.online.repository.BookRepository;
+
+public class SimpleSchoolBookService implements BookService<SchoolBook> {
+
+    private BookRepository<SchoolBook> schoolBookBookRepository;
+    private AuthorService authorService;
+
+    public SimpleSchoolBookService() {
+    }
+
+    public SimpleSchoolBookService(BookRepository<SchoolBook> schoolBookBookRepository, AuthorService authorService) {
+        this.schoolBookBookRepository = schoolBookBookRepository;
+        this.authorService = authorService;
+    }
+
+    @Override
+    public boolean save(SchoolBook book) {
+        if (authorService.findByFullName(book.getAuthorName(), book.getAuthorLastName()) != null) {
+            return schoolBookBookRepository.save(book);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public SchoolBook[] findByName(String name) {
+        return schoolBookBookRepository.findByName(name);
+    }
+
+    @Override
+    public int getNumberOfBooksByName(String name) {
+        return schoolBookBookRepository.findByName(name).length;
+    }
+
+    @Override
+    public boolean removeByName(String name) {
+        return schoolBookBookRepository.removeByName(name);
+    }
+
+    @Override
+    public int count() {
+        return schoolBookBookRepository.count();
+    }
+
+    @Override
+    public Author findAuthorByBookName(String name) {
+        SchoolBook[] books = schoolBookBookRepository.findByName(name);
+        for (SchoolBook book : books) {
+            return authorService.findByFullName(book.getAuthorName(), book.getAuthorLastName());
+        }
+        return null;
+    }
+}
