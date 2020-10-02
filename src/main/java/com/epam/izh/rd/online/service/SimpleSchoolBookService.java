fe949d591e8a28441cc86d62a5c117ae5b0dd5f8
Ivan_Babushkin <babushkinnavi.ib@gmail.com>
2020-10-02 16:29:20 +0300

    Tests passed

---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
@@ -0,0 +1,60 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.entity.SchoolBook;
+import com.epam.izh.rd.online.repository.BookRepository;
+
+public class SimpleSchoolBookService implements BookService<SchoolBook> {
+
+    private BookRepository<SchoolBook> schoolBookBookRepository;
+    private AuthorService authorService;
+
+    public SimpleSchoolBookService() {
+    }
+
+    public SimpleSchoolBookService(BookRepository<SchoolBook> schoolBookBookRepository, AuthorService authorService) {
+        this.schoolBookBookRepository = schoolBookBookRepository;
+        this.authorService = authorService;
+    }
+
+    @Override
+    public boolean save(SchoolBook book) {
+        if (authorService.findByFullName(book.getAuthorName(), book.getAuthorLastName()) != null) {
+            schoolBookBookRepository.save(book);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public SchoolBook[] findByName(String name) {
+        return schoolBookBookRepository.findByName(name);
+    }
+
+    @Override
+    public int getNumberOfBooksByName(String name) {
+        SchoolBook[] schoolBooks = schoolBookBookRepository.findByName(name);
+        return schoolBooks.length;
+    }
+
+    @Override
+    public boolean removeByName(String name) {
+        return schoolBookBookRepository.removeByName(name);
+    }
+
+    @Override
+    public int count() {
+        return schoolBookBookRepository.count();
+    }
+
+    @Override
+    public Author findAuthorByBookName(String name) {
+        SchoolBook[] schoolBooks = schoolBookBookRepository.findByName(name);
+        if (schoolBooks.length > 0) {
+            String AuthorName = schoolBooks[0].getAuthorName();
+            String AuthorLastName = schoolBooks[0].getAuthorLastName();
+            return authorService.findByFullName(AuthorName, AuthorLastName);
+        }
+        return null;
+    }
+}
