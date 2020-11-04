2a55f113a6105b2ce967ae0c76274524b193a50e
Ruslan Mavlyashov <ruslan.mavlyashov@raiffeisen.ru>
2020-11-04 23:10:37 +0300

    done

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,9 +1,27 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.entity.Book;
+import com.epam.izh.rd.online.entity.SchoolBook;
+import com.epam.izh.rd.online.repository.SimpleAuthorRepository;
+import com.epam.izh.rd.online.repository.SimpleSchoolBookRepository;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        /*SimpleAuthorRepository sa = new SimpleAuthorRepository();
+        Author author = new Author();
+        author.setName("Ruslan");
+        author.setLastName("Mavlyashov");
+        sa.save(author);
+        System.out.println(author.getName() + " " + author.getLastName() + " " + author.getBirthdate());*/
 
+        SimpleSchoolBookRepository sb = new SimpleSchoolBookRepository();
+        SchoolBook schoolBooks = new SchoolBook();
+        schoolBooks.setName("Master");
+        schoolBooks.setName("Margarita");
+        sb.removeByName("Master");
     }
-
 }
--- src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -1,4 +1,61 @@
 package com.epam.izh.rd.online.repository;
 
-public class SimpleSchoolBookRepository {
+import com.epam.izh.rd.online.entity.SchoolBook;
+
+import java.util.Arrays;
+
+public class SimpleSchoolBookRepository implements BookRepository<SchoolBook> {
+
+    @Override
+    public boolean save(SchoolBook book) {
+        SchoolBook[] temp = new SchoolBook[schoolBooks.length + 1];
+        for (int i = 0; i < schoolBooks.length; i++) {
+            temp[i] = schoolBooks[i];
+        }
+        temp[schoolBooks.length] = book;
+        schoolBooks = temp;
+        return true;
+    }
+
+    @Override
+    public SchoolBook[] findByName(String name) {
+        if (schoolBooks != null) {
+            int i = 0;
+            SchoolBook[] findBook = new SchoolBook[schoolBooks.length];
+            for (SchoolBook scBook : schoolBooks) {
+                if (scBook.getName().equals(name)) {
+                    findBook[i] = scBook;
+                    i++;
+                }
+            }
+            return Arrays.copyOf(findBook, i);
+        } else return new SchoolBook[0];
+    }
+
+    @Override
+    public boolean removeByName(String name) {
+        if (schoolBooks != null) {
+            int count = 1;
+            for (int i = 0; i < schoolBooks.length; i++) {
+                if (schoolBooks[i].getName().equals(name)) {
+                    schoolBooks[i] = schoolBooks[schoolBooks.length - count];
+                    count++;
+                }
+            }
+            if (count > 1) {
+                schoolBooks = Arrays.copyOf(schoolBooks, schoolBooks.length - (count - 1));
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return schoolBooks.length;
+    }
+
+    private SchoolBook[] schoolBooks = new SchoolBook[]{};
 }
