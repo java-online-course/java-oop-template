ea8ace2939cde0463b2b79a84083177c1700d2aa
Evgeniy <nifpopnaf@gmail.com>
2021-10-28 10:57:07 +0400

    fill another class in entity package

--- src/main/java/com/epam/izh/rd/online/entity/Book.java
+++ src/main/java/com/epam/izh/rd/online/entity/Book.java
@@ -16,5 +16,53 @@ import java.util.Objects;
  * 6) Переопределить метод toString с выводом всех полей (не забывайте alt+inset)
  */
 public abstract class Book {
+    private int numberOfPages;
+    private String name;
 
+    public Book() {
+    }
+
+    public Book(int numberOfPages, String name) {
+        this.numberOfPages = numberOfPages;
+        this.name = name;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        if (numberOfPages != book.numberOfPages) return false;
+        return name.equals(book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31 * (name != null ? name.hashCode() : 0);
+        return result * numberOfPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "numberOfPages=" + numberOfPages +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
