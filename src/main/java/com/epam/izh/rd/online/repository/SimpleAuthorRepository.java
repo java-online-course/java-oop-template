16367256aaf6d7a5cc988c250c827f9395d30f62
Evgeniy <nifpopnaf@gmail.com>
2021-10-28 12:15:37 +0400

    implement authorRepository and Service

---
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -0,0 +1,96 @@
+package com.epam.izh.rd.online.repository;
+
+import com.epam.izh.rd.online.entity.Author;
+
+import java.util.Arrays;
+
+
+public class SimpleAuthorRepository implements  AuthorRepository{
+    private Author[] authors = new Author[]{};
+    private int count = 0;
+
+    /**
+     * Метод должен сохранять автора в массив authors.
+     * Массив при каждом сохранении должен увеличиваться в размере ровно на 1.
+     * То есть он ровно того размера, сколько сущностей мы в него сохранили.
+     * <p>
+     * Если на вход для сохранения приходит автор, который уже есть в массиве (сохранен), то автор не сохраняется и
+     * метод возвращает false.
+     * <p>
+     * Можно сравнивать только по полному имени (имя и фамилия), считаем, что двух авторов
+     * с одинаковыми именем и фамилией быть не может.
+     * Подсказка - можно использовать для проверки метод findByFullName.
+     * <p>
+     * Если сохранение прошло успешно, метод должен вернуть true.
+     *
+     * @param author
+     */
+    @Override
+    public boolean save(Author author) {
+        if (findByFullName(author.getName(), author.getLastName()) == author) {
+            return false;
+        }
+        authors = Arrays.copyOf(authors, authors.length * 2);
+        authors[count++] = author;
+        return true;
+    }
+
+    /**
+     * Метод должен находить в массиве authors автора по имени и фамилии (считаем, что двух авторов
+     * с одинаковыми именем и фамилией быть не может.)
+     * <p>
+     * Если автор с таким именем и фамилией найден - возвращаем его, если же не найден, метод должен вернуть null.
+     *
+     * @param name
+     * @param lastname
+     */
+    @Override
+    public Author findByFullName(String name, String lastname) {
+        if (authors.length == 0) return null;
+
+        for (int i = 0; i < count; i++) {
+            Author author = authors[i];
+            if (author.getName().equals(name) && author.getLastName().equals(lastname)) {
+                return author;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Метод должен удалять автора из массива authors, если он там имеется.
+     * Автора опять же, можно определять только по совпадению имении и фамилии.
+     * <p>
+     * Важно: при удалении автора из массива размер массива должен уменьшиться!
+     * То есть, если мы сохранили 2 авторов и вызвали count() (метод ниже), то он должен вернуть 2.
+     * Если после этого мы удалили 1 автора, метод count() должен вернуть 1.
+     * <p>
+     * Если автор был найден и удален, метод должен вернуть true, в противном случае, если автор не был найден, метод
+     * должен вернуть false.
+     *
+     * @param author
+     */
+    @Override
+    public boolean remove(Author author) {
+        if (findByFullName(author.getName(), author.getLastName()) == author) {
+            return false;
+        }
+
+        for (int i = 0; i < count; i++) {
+            if (authors[i].equals(author)) {
+                System.arraycopy(authors, i + 1, authors, i, count - 1 - i);
+                count--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Метод возвращает количество сохраненных сущностей в массиве authors.
+     */
+    @Override
+    public int count() {
+        return count;
+    }
+}
---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleAuthorService.java
@@ -0,0 +1,53 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.repository.AuthorRepository;
+
+public class SimpleAuthorService implements AuthorService{
+    private AuthorRepository authorRepository;
+
+    public SimpleAuthorService() {
+    }
+
+    public SimpleAuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    /**
+     * Метод должен сохранять автора.
+     * По факту, он просто обращается к репозиторию с авторами и вызывает аналогичный метод, псоле чего возвращает результат.
+     */
+    @Override
+    public boolean save(Author author) {
+        return this.authorRepository.save(author);
+    }
+
+    /**
+     * Метод должен находить автора по имени и фамилии.
+     * По факту, он просто обращается к репозиторию с авторами и вызывает аналогичный метод, псоле чего возвращает результат.
+     *
+     */
+    @Override
+    public Author findByFullName(String name, String lastname) {
+        return this.authorRepository.findByFullName(name, lastname);
+    }
+
+    /**
+     * Метод должен удалять автора.
+     * По факту, он просто обращается к репозиторию с авторами и вызывает аналогичный метод, псоле чего возвращает результат..
+     *
+     */
+    @Override
+    public boolean remove(Author author) {
+        return this.authorRepository.remove(author);
+    }
+
+    /**
+     * Метод считать количество сохраненных авторов на текущий момент.
+     * По факту, он просто обращается к репозиторию с авторами и вызывает аналогичный метод, псоле чего возвращает результат.
+     */
+    @Override
+    public int count() {
+        return this.authorRepository.count();
+    }
+}
